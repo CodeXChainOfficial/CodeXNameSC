48d4f37db27fbdaec8767e46baa5a583b7d3d2d0
ericobi <ericobi.lee@gmail.com>
2023-08-03 10:37:03 -0400

    basic structure from ens

---
+++ contracts/access/AdminControl.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Ownable.sol";
+import "./Roles.sol";
+
+contract AdminControl is Ownable {
+
+    using Roles for Roles.Role;
+
+    Roles.Role private _controllerRoles;
+
+
+    modifier onlyMinterController() {
+      require (
+        hasRole(msg.sender), 
+        "AdminControl: sender must has minting role"
+      );
+      _;
+    }
+
+    modifier onlyMinter() {
+      require (
+        hasRole(msg.sender), 
+        "AdminControl: sender must has minting role"
+      );
+      _;
+    }
+
+    constructor() {
+      _grantRole(msg.sender);
+    }
+
+    function grantMinterRole (address account) public  onlyOwner {
+      _grantRole(account);
+    }
+
+    function revokeMinterRole (address account) public  onlyOwner {
+      _revokeRole(account);
+    }
+
+    function hasRole(address account) public view returns (bool) {
+      return _controllerRoles.has(account);
+    }
+    
+    function _grantRole (address account) internal {
+      _controllerRoles.add(account);
+    }
+
+    function _revokeRole (address account) internal {
+      _controllerRoles.remove(account);
+    }
+
+}
