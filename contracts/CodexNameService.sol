9bf15946c1d83447f41e09de01023972b2b586c9
ericobi <ericobi.lee@gmail.com>
2023-08-07 00:11:57 -0400

    fifs registrar update

--- contracts/CodexNameService.sol
+++ contracts/CodexNameService.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.0;
 
 import "./token/TRC721Enumerable.sol";
 import "./access/WhiteList.sol";
-import "./access/BookingList.sol";
+import "./access/Ownable.sol";
 import "./storage/RecordStorage.sol";
 import "./utils/SafeMath.sol";
 import "./utils/StringUtil.sol";
 import "./utils/EnumerableSet.sol";
 
-contract CodexNameService is TRC721Enumerable, RecordStorage, WhiteList, BookingList
+contract CodexNameService is TRC721Enumerable, RecordStorage, Ownable
 {
 	using SafeMath for uint256;
 	 
@@ -30,18 +30,6 @@ contract CodexNameService is TRC721Enumerable, RecordStorage, WhiteList, Booking
     mapping(uint256 => string) private _tlds;
 	
 	string private _nftBaseURI = "";
-	
-	bool public _saleIsActive = true;
-	
-	bool public _saleTwoCharIsActive = false;
-
-	uint256 private _price = 1;
-	
-	uint256 private _2chartimes = 100;
-	
-	uint256 private _3chartimes = 20;
-	
-	uint256 private _4chartimes = 5;
 	
     modifier onlyApprovedOrOwner(uint256 tokenId) {
         require(
@@ -68,25 +56,6 @@ contract CodexNameService is TRC721Enumerable, RecordStorage, WhiteList, Booking
 	function exists(uint256 tokenId) external view returns (bool) {
         return _exists(tokenId);
     }
-
-
-	function getPrice() public view returns (uint256) {
-        return _price;
-    }
-	
-	function setTimes(uint256 _2chartimenew, uint256 _3chartimenew, uint256 _4chartimenew) public onlyOwner {
-		_2chartimes = _2chartimenew;
-        _3chartimes = _3chartimenew;
-		_4chartimes = _4chartimenew;
-    }
-	
-	function setPrice(uint256 price) public onlyOwner {
-        _price = price;
-    }
-	
-	function setSaleStateTwoChar() public onlyOwner {
-        _saleTwoCharIsActive = !_saleTwoCharIsActive;
-    }
 	
 	function setTLD(string memory _tld) public onlyOwner {
         uint256 tokenId = genTokenId(_tld);
@@ -101,10 +70,6 @@ contract CodexNameService is TRC721Enumerable, RecordStorage, WhiteList, Booking
         }
 		return isExist;
 	}
-	
-	function setSaleState() public onlyOwner {
-        _saleIsActive = !_saleIsActive;
-    }
 	
 	function _baseURI() internal view override returns (string memory) {
         return _nftBaseURI;
@@ -135,76 +100,6 @@ contract CodexNameService is TRC721Enumerable, RecordStorage, WhiteList, Booking
 	function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
         require(_exists(tokenId), "TRC721Metadata: URI set of nonexistent token");
         _tokenURIs[tokenId] = _tokenURI;
-    }
-	
-
-	function buyDomain(string memory domain, string memory tld) external payable 
-	{
-		require(_saleIsActive, "Sale must be active to buy");
-		
-		require(bytes(tld).length != 0, "Top level domain must be non-empty");
-		
-		require(isTLD(tld) == true, "Top level domain not exist");
-		
-		require(StringUtil.dotCount(domain) == 0, "Domains cannot contain dot");
-		
-		uint256 _length = bytes(domain).length;
-		
-		require(_length != 0, "Domain must be non-empty");	
-		
-		require(_length >= 2, "Domain requires at least 2 characters");	
-		
-		// Check BookingList
-		if (_isBookingListActive == true){
-			string memory domain_name = _bookingList[bytes(domain)];
-			require(bytes(domain_name).length == 0, "This name is already reserved");
-		}
-		
-		
-	    // Check WhiteList
-		if (_isWhiteListActive == true){
-			uint256 numbers = _whiteList[msg.sender];
-			require(numbers > 0, "The address is not in the Whitelist");
-			require(numbers >= balanceOf(msg.sender), "Exceeded max available to purchase");
-		}
-		
-		if (_length == 2)
-		{
-			require(_saleTwoCharIsActive == true, "2 Character domain names need to be allowed to buy");
-			
-			require(msg.value >= _price.mul(_2chartimes), "Insufficient Token or Token value sent is not correct");
-		}
-	
-		if (_length == 3)
-		{
-			require(msg.value >= _price.mul(_3chartimes), "Insufficient Token or Token value sent is not correct");
-		}
-		
-		if (_length == 4)
-		{
-			require(msg.value >= _price.mul(_4chartimes), "Insufficient Token or Token value sent is not correct");
-		}
-		
-		if (_length >= 5)
-		{
-			require(msg.value >= _price, "Insufficient Token or Token value sent is not correct");
-		}
-		
-		string memory _domain = StringUtil.toLower(domain);
-		
-		string memory _tld = StringUtil.toLower(tld);
-		
-		_domain = string(abi.encodePacked(_domain, ".", _tld));
-		
-		uint256 tokenId = genTokenId(_domain);
-		
-		require (!_exists(tokenId), "Domain already exists");
-		
-	   _safeMint(msg.sender, tokenId);
-	   
-	   _setTokenURI(tokenId, _domain);
-	   
-	   emit NewURI(tokenId, _domain);
     }
 
 	function registerDomain(address to, string memory domain, string memory tld) external onlyMinterController 
@@ -412,6 +307,7 @@ contract CodexNameService is TRC721Enumerable, RecordStorage, WhiteList, Booking
 		
         super._burn(_newTokenId);
     }
+
 	function subTokenIdCount(uint256 tokenId) public view returns (uint256) {
         require (_exists(tokenId));
         return _subDomains[tokenId].length();
@@ -425,7 +321,6 @@ contract CodexNameService is TRC721Enumerable, RecordStorage, WhiteList, Booking
      * End:Subdomain
      */
 
-  
 	/**
      * Begin: System
      */
--- contracts/FIFSRegistrar.sol
+++ contracts/FIFSRegistrar.sol
@@ -2,7 +2,112 @@
 
 pragma solidity ^0.8.0;
 
-contract FIFSRegistrar
+import "./access/WhiteList.sol";
+import "./access/BookingList.sol";
+import "./utils/StringUtil.sol";
+
+contract FIFSRegistrar is WhiteList, BookingList
 {
+    bool public _saleIsActive = true;
+	
+	bool public _saleTwoCharIsActive = false;
+
+	uint256 private _price = 1;
+	
+	uint256 private _2chartimes = 100;
+	
+	uint256 private _3chartimes = 20;
+	
+	uint256 private _4chartimes = 5;
+
+	function getPrice() public view returns (uint256) {
+        return _price;
+    }
+	
+	function setTimes(uint256 _2chartimenew, uint256 _3chartimenew, uint256 _4chartimenew) public onlyOwner {
+		_2chartimes = _2chartimenew;
+        _3chartimes = _3chartimenew;
+		_4chartimes = _4chartimenew;
+    }
+	
+	function setPrice(uint256 price) public onlyOwner {
+        _price = price;
+    }
+	
+	function setSaleStateTwoChar() public onlyOwner {
+        _saleTwoCharIsActive = !_saleTwoCharIsActive;
+    }
     
+	function setSaleState() public onlyOwner {
+        _saleIsActive = !_saleIsActive;
+    }
+
+    function buyDomain(string memory domain, string memory tld) external payable 
+	{
+		require(_saleIsActive, "Sale must be active to buy");
+		
+		require(bytes(tld).length != 0, "Top level domain must be non-empty");
+		
+		require(isTLD(tld) == true, "Top level domain not exist");
+		
+		require(StringUtil.dotCount(domain) == 0, "Domains cannot contain dot");
+		
+		uint256 _length = bytes(domain).length;
+		
+		require(_length != 0, "Domain must be non-empty");	
+		
+		require(_length >= 2, "Domain requires at least 2 characters");	
+		
+		// Check BookingList
+		if (_isBookingListActive == true){
+			string memory domain_name = _bookingList[bytes(domain)];
+			require(bytes(domain_name).length == 0, "This name is already reserved");
+		}
+		
+		
+	    // Check WhiteList
+		if (_isWhiteListActive == true){
+			uint256 numbers = _whiteList[msg.sender];
+			require(numbers > 0, "The address is not in the Whitelist");
+			require(numbers >= balanceOf(msg.sender), "Exceeded max available to purchase");
+		}
+		
+		if (_length == 2)
+		{
+			require(_saleTwoCharIsActive == true, "2 Character domain names need to be allowed to buy");
+			
+			require(msg.value >= _price.mul(_2chartimes), "Insufficient Token or Token value sent is not correct");
+		}
+	
+		if (_length == 3)
+		{
+			require(msg.value >= _price.mul(_3chartimes), "Insufficient Token or Token value sent is not correct");
+		}
+		
+		if (_length == 4)
+		{
+			require(msg.value >= _price.mul(_4chartimes), "Insufficient Token or Token value sent is not correct");
+		}
+		
+		if (_length >= 5)
+		{
+			require(msg.value >= _price, "Insufficient Token or Token value sent is not correct");
+		}
+		
+		string memory _domain = StringUtil.toLower(domain);
+		
+		string memory _tld = StringUtil.toLower(tld);
+		
+		_domain = string(abi.encodePacked(_domain, ".", _tld));
+		
+		uint256 tokenId = genTokenId(_domain);
+		
+		require (!_exists(tokenId), "Domain already exists");
+		
+	   _safeMint(msg.sender, tokenId);
+	   
+	   _setTokenURI(tokenId, _domain);
+	   
+	   emit NewURI(tokenId, _domain);
+    }
 }
